695102b6c02cbebfea836cd2d08d3fcf41c6a505
MSAC98 <44638120+MSAC98@users.noreply.github.com>
2023-01-06 16:53:12 +0100

    små ændringer i triplist

---
+++ app/src/main/java/com/project/oplevapp/model/TripInfo.kt
@@ -0,0 +1,11 @@
+package com.project.oplevapp.model
+
+import android.media.Image
+import android.widget.Button
+import androidx.navigation.NavController
+
+data class TripInfo(
+    val name: String,
+    val image: Int,
+    //val button: Unit,
+)
