7a56b5b2ce5d4b5f3f2aaa72c7565781ca135d6c
s185736 <65380030+s185736@users.noreply.github.com>
2023-01-07 15:42:57 +0100

    Updated Profile.

--- app/src/main/java/com/project/oplevapp/ui/screen/profile/Profile.kt
+++ app/src/main/java/com/project/oplevapp/ui/screen/profile/Profile.kt
@@ -3,30 +3,28 @@ package com.project.oplevapp.ui.screen.profile
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.project.oplevapp.R
-import com.project.oplevapp.ui.screen.country.MyTextField
-
+import com.project.oplevapp.ui.shared.components.MyTextField
+import com.project.oplevapp.ui.shared.components.UneditableTextField
+import com.project.oplevapp.ui.theme.LightRed
 
 @Preview(showBackground = true)
 @Composable
@@ -50,8 +48,17 @@ fun Profile() {
 
                     Spacer(modifier = Modifier.padding(start = 200.dp))
 
-                    Icon(imageVector = ImageVector.vectorResource(id = R.drawable.ic_outline_logout_24), contentDescription = "")
-
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        IconButton(
+                            onClick = {/**TODO**/},
+                            modifier = Modifier.size(50.dp)) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_outline_logout_24),
+                                contentDescription = "Slet Ide.",
+                                tint = LightRed,
+                            )
+                        }
+                    }
 
 
                 }
@@ -61,21 +68,16 @@ fun Profile() {
 
 
                 var email by remember {
-                    mutableStateOf("")
+                    mutableStateOf("Hans@mail.com")
                 }
-
                 var password by remember {
-                    mutableStateOf("")
+                    mutableStateOf("123456")
                 }
                 var Name by remember {
-                    mutableStateOf("")
+                    mutableStateOf("Hans Ole")
                 }
                 var phone by remember {
-                    mutableStateOf("")
-                }
-
-                var confirmPassword by remember {
-                    mutableStateOf("")
+                    mutableStateOf("42424242")
                 }
 
                 MyTextField(
@@ -94,14 +96,14 @@ fun Profile() {
                 )
                 Spacer(modifier = Modifier.padding(bottom = 27.dp))
 
-                MyTextField(
+                UneditableTextField(
                     text = email,
                     textSize = 15,
                     onValueChange = { email = it },
                     placeHolder = "Email",
                     width = 320,
                     height = 57,
-                    KeyboardType.Text,
+                    KeyboardType.Email,
                     visualTransformation = VisualTransformation.None,
                     Color.DarkGray,
                     Color.LightGray,
@@ -120,7 +122,7 @@ fun Profile() {
                     placeHolder = "Telefon",
                     width = 320,
                     height = 57,
-                    KeyboardType.Text,
+                    KeyboardType.Phone,
                     visualTransformation = VisualTransformation.None,
                     Color.DarkGray,
                     Color.LightGray,
@@ -136,8 +138,8 @@ fun Profile() {
                     placeHolder = "Adgangskode",
                     width = 320,
                     height = 57,
-                    KeyboardType.Text,
-                    visualTransformation = VisualTransformation.None,
+                    KeyboardType.Password,
+                    visualTransformation = PasswordVisualTransformation(),
                     Color.DarkGray,
                     Color.LightGray,
                     Color.Gray,
@@ -150,54 +152,49 @@ fun Profile() {
                 Spacer(modifier = Modifier.padding(bottom = 300.dp))
 
 
-
+            Row {
                 Button(
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(5,54,103)),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(5, 54, 103)),
                     shape = RoundedCornerShape(60),
                     modifier = Modifier
                         .height(45.dp)
                         .width(189.dp),
-
-
                     onClick = { /** TO DO */ },
 
                     ) {
-                    Text("Opdater",
+                    Text(
+                        "Opdater",
                         color = Color.White,
-                        fontSize = 16.sp)
+                        fontSize = 16.sp
+                    )
 
 
                 }
+            }
+                Spacer(modifier = Modifier.padding(start = 200.dp))
+
+                Row {
+                    Button(
+                        colors = ButtonDefaults.buttonColors(backgroundColor = LightRed),
+                        shape = RoundedCornerShape(60),
+                        modifier = Modifier
+                            .height(45.dp)
+                            .width(189.dp),
+                        onClick = { /** TO DO */ },
+
+                        ) {
+                        Text(
+                            "Fortryd",
+                            color = Color.White,
+                            fontSize = 16.sp
+                        )
 
 
+                    }
+                }
 
 
             }
-        }
-    }
-
-
-    @Composable
-    fun UpdateButton() {
-        Button(
-            onClick = {},
-            shape = RoundedCornerShape(60),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color(5,54,103)),
-            modifier = Modifier
-                .height(38.dp)
-                .width(130.dp)
-        ) {
-            Row {
-                Text(
-                    text = "Opdater",
-                    color = Color.White,
-                    fontSize = 12.sp,
-                    modifier = Modifier.padding()
-                )
-
-            }
-
         }
     }
 }
-
--- app/src/main/java/com/project/oplevapp/ui/theme/profilePage/ProfileSettingsActivity.kt
+++ app/src/main/java/com/project/oplevapp/ui/theme/profilePage/ProfileSettingsActivity.kt
@@ -1,111 +1,250 @@
 package com.project.oplevapp.ui.theme.profilePage;
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.Image
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import com.project.oplevapp.ui.theme.OplevAppTheme
-import androidx.compose.ui.res.*
-import androidx.compose.ui.unit.dp
-import com.project.oplevapp.ui.Utility.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.runtime.snapshots.readable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.project.oplevapp.R
+import com.project.oplevapp.ui.shared.components.MyTextField
 
-class ProfileSettingsActivity : ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                setContent {
-                        OplevAppTheme() {
-                                ProfilePage()
-                        }
-                }
-        }
-}
-
+@Preview(showBackground = true)
 @Composable
-private fun ProfileIcon(modifier: Modifier = Modifier) {
-        Surface(modifier = modifier.padding(8.dp), shape = CircleShape, elevation = 2.dp) {
-              /*  Image(painter = painterResource(id = R.drawable.profile_image),
-                        contentDescription = stringResource(id = R.String.profile_image)
-                )
+fun ProfileSettingsActivity() {
+        Scaffold {
+                Box {
 
-               */
-        }
-}
+                        Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.padding(27.dp)
+                        ) {
+                                Text(
+                                        text = "Profil",
+                                        color = Color(5, 54, 103),
+                                        fontWeight = FontWeight.ExtraBold,
+                                        fontSize = 25.sp,
+                                        textAlign = TextAlign.Center
+                                )
 
-@Composable
-fun ProfilePage() {
-        var isInPage: Boolean by remember {
-                mutableStateOf(false)
-        }
-        Surface(modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()) {
-                Card(modifier = Modifier.padding(70.dp), elevation = 20.dp, shape = MaterialTheme.shapes.medium) {
-                        Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
-                                ProfileIcon(modifier = Modifier.size(160.dp))
-                                Text("Hans Hansen", style = MaterialTheme.typography.h4)
-                                Text(text = "Jeg glæder mig!", style = MaterialTheme.typography.h5,)
-                                Button(onClick = { isInPage = !isInPage }) {
-                                        Text(text = "Min Profil")
+                                Spacer(modifier = Modifier.padding(bottom = 30.dp))
+
+
+                                var email by remember {
+                                        mutableStateOf("")
                                 }
-                                if (!isInPage) {
-                                } else {
-                                        ProfileBox(showUserProfileInfo())
-                                }
-                        }
-                }
-        }
-}
 
-@Composable
-fun ProfileBox(settings: List<ProfileSetting>) {
-        Box {
-                Surface() {
-                        Column {
-                                LazyColumn {
-                                        items(settings) { setting ->
-                                                ProfileBoxLayout(setting)
+                                var password by remember {
+                                        mutableStateOf("")
+                                }
+                                var Name by remember {
+                                        mutableStateOf("")
+                                }
+                                var phone by remember {
+                                        mutableStateOf("")
+                                }
+
+
+                                MyTextField(
+                                        text = Name,
+                                        textSize = 15,
+                                        onValueChange = { Name = it },
+                                        placeHolder = "Navn",
+                                        width = 320,
+                                        height = 57,
+                                        KeyboardType.Text,
+                                        visualTransformation = VisualTransformation.None,
+                                        Color.DarkGray,
+                                        Color.LightGray,
+                                        Color.Gray,
+                                        vectorPainter = painterResource(id = R.drawable.ic_outline_person_24),
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                                MyTextField(
+                                        text = email,
+                                        textSize = 15,
+                                        onValueChange = { email = it },
+                                        placeHolder = "Email",
+                                        width = 320,
+                                        height = 57,
+                                        KeyboardType.Text,
+                                        visualTransformation = VisualTransformation.None,
+                                        Color.DarkGray,
+                                        Color.LightGray,
+                                        Color.Gray,
+                                        vectorPainter = painterResource(id = R.drawable.ic_outline_mail_outline_24),
+                                )
+
+
+
+                                Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                                ProfileTextField(
+                                        text = phone,
+                                        textSize = 15,
+                                        onValueChange = { phone = it },
+                                        placeHolder = "Telefon",
+                                        width = 320,
+                                        height = 57,
+                                        KeyboardType.Text,
+                                        visualTransformation = VisualTransformation.None,
+                                        Color.DarkGray,
+                                        Color.LightGray,
+                                        Color.Gray,
+                                        vectorPainter = painterResource(id = R.drawable.ic_outline_phone_24)
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                                MyTextField(
+                                        text = password,
+                                        textSize = 15,
+                                        onValueChange = { password = it },
+                                        placeHolder = "Adgangskode",
+                                        width = 320,
+                                        height = 57,
+                                        KeyboardType.Text,
+                                        visualTransformation = VisualTransformation.None,
+                                        Color.DarkGray,
+                                        Color.LightGray,
+                                        Color.Gray,
+                                        vectorPainter = painterResource(id = R.drawable.ic_outline_vpn_key_24)
+                                )
+                                Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                                Column(
+                                        // we are using column to align our
+                                        // imageview to center of the screen.
+                                        modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+
+                                        // below line is used for specifying
+                                        // vertical arrangement.
+                                        verticalArrangement = Arrangement.Center,
+
+                                        // below line is used for specifying
+                                        // horizontal arrangement.
+                                        horizontalAlignment = Alignment.CenterHorizontally,
+                                ) {
+                                        // below line is use to get
+                                        // the context for our app.
+                                        val context = LocalContext.current
+
+                                        // below line is use to create a button.
+                                        Button(
+                                                // below line is use to add onclick
+                                                // parameter for our button onclick
+                                                onClick = {
+                                                        // when user is clicking the button
+                                                        // we are displaying a toast message.
+                                                        Toast.makeText(context, "Welcome to Geeks for Geeks", Toast.LENGTH_LONG)
+                                                                .show()
+                                                },
+                                                // in below line we are using modifier
+                                                // which is use to add padding to our button
+                                                modifier = Modifier.padding(all = Dp(10F)),
+
+                                                // below line is use to set or
+                                                // button as enable or disable.
+                                                enabled = true,
+
+                                                // below line is use to
+                                                // add border to our button.
+                                                border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Blue)),
+
+                                                // below line is use to add shape for our button.
+                                                shape = MaterialTheme.shapes.medium,
+                                        )
+                                        // below line is use to
+                                        // add text on our button
+                                        {
+                                                Text(text = "Geeks for Geeks", color = Color.White)
                                         }
                                 }
                         }
                 }
         }
-
 }
 
 @Composable
-fun ProfileBoxLayout(setting: ProfileSetting) {
-        Card(modifier = Modifier
-                .fillMaxWidth()
-                .padding(50.dp),
-                shape = RectangleShape, elevation = 10.dp) {
-                Row(modifier = Modifier.padding(start = 20.dp),
-                        verticalAlignment = Alignment.CenterVertically) {
-                        Column(horizontalAlignment = Alignment.Start) {
-                                Text(text = setting.name)
-                                Text(text = setting.value)
-                        }
+fun ProfileTextField(
+        text: String,
+        textSize: Int,
+        onValueChange: (String) -> Unit,
+        placeHolder: String,
+        width: Int,
+        height: Int,
+        keyboardType: KeyboardType,
+        visualTransformation: VisualTransformation,
+        myTextColor: Color,
+        backgroundColor: Color,
+        placeHolderColor: Color,
+        vectorPainter: Painter
+) {
+        Surface(
+                modifier = Modifier.size(width.dp, height.dp),
+                color = Color.White,
+                shape = RoundedCornerShape(35),
+        ) {
+                Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth()
+                ) {
+
+
+                        TextField(
+
+                                value = text,
+                                onValueChange = onValueChange,
+                                textStyle = LocalTextStyle.current.copy(color = myTextColor),
+                                placeholder = {
+                                        Text(
+                                                text = placeHolder,
+                                                fontSize = textSize.sp,
+                                                fontWeight = FontWeight.Bold,
+                                                fontFamily = FontFamily.Default,
+                                                textAlign = TextAlign.Left,
+                                                color = placeHolderColor,
+                                                modifier = Modifier
+                                                        .fillMaxWidth()
+                                                        .height(height = (height + 50).dp)
+                                        )
+
+                                },
+                                leadingIcon = {Icon(painter = vectorPainter, contentDescription ="" ) },
+                                visualTransformation = visualTransformation,
+                                keyboardOptions = KeyboardOptions(
+                                        keyboardType = keyboardType
+                                ),
+
+                                modifier = Modifier.fillMaxSize(),
+
+                                colors = TextFieldDefaults.textFieldColors(
+                                        disabledTextColor = Color.Transparent,
+                                        backgroundColor = backgroundColor,
+                                        focusedIndicatorColor = Color.Transparent,
+                                        unfocusedIndicatorColor = Color.Transparent,
+                                        disabledIndicatorColor = Color.Transparent
+                                ),
+                                readOnly = true
+                        )
                 }
-        }
-}
-
-fun showUserProfileInfo(): List<ProfileSetting> =
-        listOf(ProfileSetting("Hans Hansen", "Hans@Hansen.dk"))
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview1() {
-        OplevAppTheme() {
-                ProfilePage()
         }
 }
