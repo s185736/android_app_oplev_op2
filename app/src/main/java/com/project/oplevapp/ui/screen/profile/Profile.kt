e85d3806be627d32fb37e8a52c7a5e89af39f38d
s185736 <65380030+s185736@users.noreply.github.com>
2023-01-08 00:17:06 +0100

    Updated Profile screen.

--- app/src/main/java/com/project/oplevapp/ui/screen/profile/Profile.kt
+++ app/src/main/java/com/project/oplevapp/ui/screen/profile/Profile.kt
@@ -1,17 +1,19 @@
 package com.project.oplevapp.ui.screen.profile
 
 
-import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,8 +24,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.project.oplevapp.MainActivity
 import com.project.oplevapp.R
+import com.project.oplevapp.ui.shared.components.BlackPreviousButton
 import com.project.oplevapp.ui.shared.components.MyTextField
 import com.project.oplevapp.ui.shared.components.UneditableTextField
 import com.project.oplevapp.ui.theme.LightRed
@@ -33,30 +35,29 @@ import com.project.oplevapp.ui.theme.LightRed
 fun Profile() {
     Scaffold {
         Box {
-
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.padding(27.dp)
             ) {
                 Row {
-
+                    BlackPreviousButton({})
                     Text(
                         text = "Profil",
                         color = Color(5, 54, 103),
                         fontWeight = FontWeight.ExtraBold,
-                        fontSize = 25.sp,
+                        fontSize = 35.sp,
                         textAlign = TextAlign.Center
                     )
-
-                    Spacer(modifier = Modifier.padding(start = 200.dp))
+                    Spacer(modifier = Modifier.padding(start = 140.dp))
 
                     Row(modifier = Modifier.fillMaxWidth()) {
                         IconButton(
-                            onClick = {/**TODO**/},
-                            modifier = Modifier.size(50.dp)) {
+                            onClick = { /**TODO**/ },
+                            modifier = Modifier.size(67.dp)
+                        ) {
                             Icon(
                                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_outline_logout_24),
-                                contentDescription = "Slet Ide.",
+                                contentDescription = "Slet Bruger.",
                                 tint = LightRed,
                             )
                         }
@@ -66,8 +67,10 @@ fun Profile() {
                 }
 
 
-                Spacer(modifier = Modifier.padding(bottom = 30.dp))
+                Spacer(modifier = Modifier.padding(bottom = 1.dp))
 
+
+                CreateImageProfile()
 
                 var email by remember {
                     mutableStateOf("Hans@mail.com")
@@ -82,84 +85,133 @@ fun Profile() {
                     mutableStateOf("42424242")
                 }
 
-                MyTextField(
-                    text = Name,
-                    textSize = 15,
-                    onValueChange = { Name = it },
-                    placeHolder = "Navn",
-                    width = 320,
-                    height = 57,
-                    KeyboardType.Text,
-                    visualTransformation = VisualTransformation.None,
-                    Color.DarkGray,
-                    Color.LightGray,
-                    Color.Gray,
-                    vectorPainter = painterResource(id = R.drawable.ic_outline_person_24),
-                )
-                Spacer(modifier = Modifier.padding(bottom = 27.dp))
-
-                UneditableTextField(
-                    text = email,
-                    textSize = 15,
-                    onValueChange = { email = it },
-                    placeHolder = "Email",
-                    width = 320,
-                    height = 57,
-                    KeyboardType.Email,
-                    visualTransformation = VisualTransformation.None,
-                    Color.DarkGray,
-                    Color.LightGray,
-                    Color.Gray,
-                    vectorPainter = painterResource(id = R.drawable.ic_outline_mail_outline_24),
-                )
-
-
-
-                Spacer(modifier = Modifier.padding(bottom = 27.dp))
-
-                UneditableTextField(
-                    text = phone,
-                    textSize = 15,
-                    onValueChange = { phone = it },
-                    placeHolder = "Telefon",
-                    width = 320,
-                    height = 57,
-                    KeyboardType.Phone,
-                    visualTransformation = VisualTransformation.None,
-                    Color.DarkGray,
-                    Color.LightGray,
-                    Color.Gray,
-                    vectorPainter = painterResource(id = R.drawable.ic_outline_phone_24)
-                )
-                Spacer(modifier = Modifier.padding(bottom = 27.dp))
-
-                MyTextField(
-                    text = password,
-                    textSize = 15,
-                    onValueChange = { password = it },
-                    placeHolder = "Adgangskode",
-                    width = 320,
-                    height = 57,
-                    KeyboardType.Password,
-                    visualTransformation = PasswordVisualTransformation(),
-                    Color.DarkGray,
-                    Color.LightGray,
-                    Color.Gray,
-                    vectorPainter = painterResource(id = R.drawable.ic_outline_vpn_key_24)
-                )
-                }
-            Spacer(modifier = Modifier.padding(bottom = 300.dp))
-
-            Button(
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color(5,54,103)),
-                modifier = Modifier.padding(70.dp) ,
-                onClick = { /** TO DO */ },
-
+                Column(
+                    modifier = Modifier
+                        .padding(5.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                Text("Opret")
+                    Text(
+                        color = Color.Blue,
+                        fontSize = 24.sp,
+                        style = MaterialTheme.typography.h4,
+                        text = Name
+                    )
+                    Spacer(modifier = Modifier.padding(bottom = 1.dp))
 
+                    MyTextField(
+                        text = Name,
+                        textSize = 15,
+                        onValueChange = { Name = it },
+                        placeHolder = "Navn",
+                        width = 320,
+                        height = 57,
+                        KeyboardType.Text,
+                        visualTransformation = VisualTransformation.None,
+                        Color.DarkGray,
+                        Color.LightGray,
+                        Color.Gray,
+                        vectorPainter = painterResource(id = R.drawable.ic_outline_person_24),
+                    )
+                    Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                    UneditableTextField(
+                        text = email,
+                        textSize = 15,
+                        onValueChange = { email = it },
+                        placeHolder = "Email",
+                        width = 320,
+                        height = 57,
+                        KeyboardType.Email,
+                        visualTransformation = VisualTransformation.None,
+                        Color.DarkGray,
+                        Color.LightGray,
+                        Color.Gray,
+                        vectorPainter = painterResource(id = R.drawable.ic_outline_mail_outline_24),
+                    )
+
+
+
+                    Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                    UneditableTextField(
+                        text = phone,
+                        textSize = 15,
+                        onValueChange = { phone = it },
+                        placeHolder = "Telefon",
+                        width = 320,
+                        height = 57,
+                        KeyboardType.Phone,
+                        visualTransformation = VisualTransformation.None,
+                        Color.DarkGray,
+                        Color.LightGray,
+                        Color.Gray,
+                        vectorPainter = painterResource(id = R.drawable.ic_outline_phone_24)
+                    )
+                    Spacer(modifier = Modifier.padding(bottom = 27.dp))
+
+                    MyTextField(
+                        text = password,
+                        textSize = 15,
+                        onValueChange = { password = it },
+                        placeHolder = "Adgangskode",
+                        width = 320,
+                        height = 57,
+                        KeyboardType.Password,
+                        visualTransformation = PasswordVisualTransformation(),
+                        Color.DarkGray,
+                        Color.LightGray,
+                        Color.Gray,
+                        vectorPainter = painterResource(id = R.drawable.ic_outline_vpn_key_24)
+                    )
+                    Spacer(modifier = Modifier.padding(bottom = 100.dp))
+                    Row {
+                        Button(
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(
+                                    5,
+                                    54,
+                                    103
+                                )
+                            ),
+                            shape = RoundedCornerShape(60),
+                            modifier = Modifier
+                                .height(45.dp)
+                                .width(189.dp),
+                            onClick = { /** TO DO */ },
+
+                            ) {
+                            Text(
+                                "Opdater",
+                                color = Color.White,
+                                fontSize = 16.sp
+                            )
+
+
+                        }
+                    }
+                }
             }
+        }
+    }
+}
+    @Composable
+    private fun CreateImageProfile(modifier: Modifier = Modifier) {
+        Surface(
+            modifier = Modifier
+                .size(100.dp)
+                .padding(5.dp),
+            shape = CircleShape,
+            border = BorderStroke(0.5.dp, Color.LightGray),
+            elevation = 4.dp,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.transparent_logo),
+                contentDescription = "profil billede.",
+                modifier = modifier.size(100.dp),
+                contentScale = ContentScale.Crop
+            )
 
         }
-   }
-}
+    }
