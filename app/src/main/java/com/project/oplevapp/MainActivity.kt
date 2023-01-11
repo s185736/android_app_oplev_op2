46cc885922b1fac36f6cd1dff8f5efc0cdb3ffe5
Alirazaakhtar <71668618+Alirazaakhtar@users.noreply.github.com>
2023-01-11 10:41:21 +0100

    azmis

--- app/src/main/java/com/project/oplevapp/MainActivity.kt
+++ app/src/main/java/com/project/oplevapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             OplevAppTheme {
                 MainNavHost()
-
+                println("Hello")
             }
         }
     }
