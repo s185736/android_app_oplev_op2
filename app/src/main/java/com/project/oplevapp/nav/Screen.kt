23340d8d787d5bb25ac3b1e0d4172aa2c0c5cdc2
s185736 <65380030+s185736@users.noreply.github.com>
2023-01-19 23:40:18 +0100

    Updated Navigation (working properly now, and navbar-hide/show method implemented).

--- app/src/main/java/com/project/oplevapp/nav/Screen.kt
+++ app/src/main/java/com/project/oplevapp/nav/Screen.kt
@@ -1,34 +1,23 @@
 package com.project.oplevapp.nav
 
-import android.graphics.drawable.Icon
-import androidx.annotation.StringRes
-import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notes
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
-import com.project.oplevapp.R
-
-sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector?) {
-
-    object ResetPassword : Screen("Nulstil Adgangskode", R.string.resetpassword, null)
-
-    object Profile : Screen("profile", R.string.profile, Icons.Default.Person)
-
-    object LandingPage : Screen("landingpage", R.string.landingpage, null)
-
-    object CountriesList : Screen("countrieslist", R.string.country_list, null)
-    object Country : Screen("country_page", R.string.country, null)
-    object EditCountry: Screen("edit_country", R.string.edit_country, null)
-    object Login: Screen("login", R.string.login, Icons.Default.Login)
-    object Note: Screen("writenotes", R.string.notes, Icons.Default.Notes)
-    object AddCountry: Screen("add_country", R.string.add_country, null)
-    object TripList: Screen("trip_list", R.string.start_page, Icons.Default.Home)
-    object CreateAccount: Screen("create_account", R.string.create_account, null)
-    object IdeaScreen: Screen("idea_portal_screen", R.string.idea_portal_screen, null)
-    object ModifyInIdeaMessageScreen: Screen("create_idea_screen", R.string.create_idea_screen, null)
-    object TripShare: Screen("trip_share", R.string.trip_share,null)
-
-    object User: Screen("user", R.string.start_page, null)
-
+sealed class Screen(var route: String, var icon: ImageVector, var title: String) {
+    object ResetPassword : Screen("reset_password", Icons.Default.Home, "Nulstil Adgangskode")
+    object Profile : Screen("profile", Icons.Default.Person, "Profil")
+    object LandingPage : Screen("landingpage", Icons.Default.Home, "Landing Page")
+    object CountriesList : Screen("countrieslist", Icons.Default.Home, "Countries List")
+    object Country : Screen("country_page",Icons.Default.Home, "Country Page")
+    object EditCountry: Screen("edit_country", Icons.Default.Home, "Edit Trip")
+    object Login: Screen("login", Icons.Default.Home, "Login")
+    object Note: Screen("write_notes", Icons.Default.Notes, "Noter")
+    object AddCountry: Screen("add_country", Icons.Default.Home, "Add Trip")
+    object TripList: Screen("trip_list", Icons.Default.Home, "Shareboard")
+    object CreateAccount: Screen("create_account", Icons.Default.Home, "Opret Konto")
+    object IdeaScreen: Screen("idea_portal_screen", Icons.Default.Home, "Ide Portalen")
+    object ModifyInIdeaMessageScreen: Screen("create_idea_screen", Icons.Default.Home, "Opret Ide")
+    object TripShare: Screen("trip_share", Icons.Default.Home, "Share Trip")
 }
